9d44d419f9acfe5f7fd8718b9fc7f763e1bd815c
unknown <you@example.com>
2022-03-28 16:58:13 +0200

    change90ll

--- o.php
+++ o.php
@@ -44,8 +44,16 @@
             echo '<td>' . $row_user['date'] . '</td>';
             echo '<td>' . $row_user['name'] . '</td>';
             echo '<td>' . $row_user['room'] . '</td>';
-            echo '<td>' . $row_user['staus'] . '<a data-status="' . $row_user['staus'] . '" data-id="' . $row_user['order_id'] . '" class="btn btn-warning ms-3" 
-            onclick="updateStatus(event)">' . $row_user["staus"] . '</a>' . '</td>';
+            echo '<td>' . $row_user['staus'] . '
+
+<select name="order" id="order">
+  <option value="process">process</option>
+  <option value="delivered">delivered</option>
+  
+</select>'.
+'<input type="submit" name="update" value="update">'
+. '</td>';
+
             echo '</tr>';
             echo '</table>';
             echo '</div>';
@@ -60,25 +68,28 @@
             $result_product = $stmt_product->fetchAll(PDO::FETCH_ASSOC);
             //$total = 0;
             foreach ($result_product as $row_product) {
-                if ($row_product['tea'] > 0) {
-                    // echo "<img src='https://fakeimg.pl/100x50/adb5bd/' class='card-img-top'>";
-                    echo "tea: " . $row_product['tea'] . "  Total: " . $row_product['tea'] * 7 . "<br>";
+            
+                if ($row_product['tea'] > 0 ) {
+                    echo "<img src='https://fakeimg.pl/100x50/adb5bd/'  >.<br>";
+                    echo " number of tea: " . $row_product['tea'] ."<br>". "  Total: " . $row_product['tea'] * 7 . "<br>";
                 }
+
                 if ($row_product['coffe'] > 0) {
-                    //echo "<img src='https://fakeimg.pl/100x50/adb5bd/' class='card-img-top'>";
-                    echo "coffe: " . $row_product['coffe'] . "  Total: " . $row_product['coffe'] * 20 . "<br>";
+                    echo "<img src='https://fakeimg.pl/100x50/adb5bd/'>.<br>";
+                    echo " number of coffe: " . $row_product['coffe'] . "<br>"."  Total: " . $row_product['coffe'] * 20 . "<br>";
                 }
                 if ($row_product['soft'] > 0) {
-                    // echo "<img src='https://fakeimg.pl/100x50/adb5bd/' class='card-img-top'>";
-                    echo "soft drink: " . $row_product['soft'] . "  Total: " . $row_product['soft'] * 7 . "<br>";
+                    
+                    echo "<img src='https://fakeimg.pl/100x50/adb5bd/' >.<br>";
+                    echo " number of soft drink: " . $row_product['soft'] ."<br>". "  Total: " . $row_product['soft'] * 7 . "<br>";
                 }
                 if ($row_product['french'] > 0) {
-                    // echo "<img src='https://fakeimg.pl/100x50/adb5bd/' class='card-img-top'>";
-                    echo "french: " . $row_product['french'] . "  Total: " . $row_product['french'] * 30 . "<br>";
+                    echo "<img src='https://fakeimg.pl/100x50/adb5bd/'>.<br>";
+                    echo "number of french: " . $row_product['french'] ."<br>". "  Total: " . $row_product['french'] * 30 . "<br>";
                 }
                 echo '</div>
                 <div class="footer">
-                    <span style="bold">Total Price: EGP ';
+                    <span style="font-size:200%  ; color:red">Total Price: EGP ';
                 echo $row_product['total'];
                 echo '</span>
                 </div>';
@@ -89,23 +100,38 @@
         echo 'Connection failed: ' . $e->getMessage();
     }
     ?>
-    <script>
-        async function updateStatus(e) {
-            let staus = e.target.dataset.staus == "processing" ? "On Delivery" : "Delivered";
-            let id = e.target.dataset.id;
-            console.log(staus, id);
-            fetch(`./controllers/changeOrderStatus.php?orderId=${id}&staus=${staus}`)
-                .then(() => {
-                    if (staus == "On Delivery")
-                        e.target.parentElement.innerHTML = `
-                        <td>${staus}<button data-status="${staus}" data-id="${id}"
-                         class="btn btn-success ms-3" onclick="updateStatus(event)">Done</button></td>`
-                    else
-                        e.target.parentElement.innerHTML = `<td>${staus}</td>`
-                })
-
+    <!-- <script>
+        // async function updateStatus(e) {
+        //     let staus = e.target.dataset.staus == "processing" ? "On Delivery" : "Delivered";
+        //     let id = e.target.dataset.id;
+        //     console.log(staus, id);
+        //     fetch(`./controllers/changeOrderStatus.php?orderId=${id}&staus=${staus}`)
+        //         .then(() => {
+        //             if (staus == "On Delivery")
+        //                 e.target.parentElement.innerHTML = `
+        //                 <td>${staus}<button data-status="${staus}" data-id="${id}"
+        //                  class="btn btn-success ms-3" onclick="updateStatus(event)">Done</button></td>`
+        //             else
+        //                 e.target.parentElement.innerHTML = `<td>${staus}</td>`
+        //         })
+        
         }
-    </script>
+    </script> -->
+   
 </body>
 
 </html>
+<?php
+    if(isset($_POST['update'])){
+        
+        $dsn = 'mysql:dbname=cafeteria;host=127.0.0.1;port=3306;';
+        $user = 'root';
+        $password = '';
+        
+            $conn = new PDO($dsn, $user, $password);
+            $sql_user = "UPDATE orders SET staus = '$value'";
+            $stmt_user = $conn->prepare($sql_user);
+            $stmt_user->execute();
+            // $result_user = $stmt_user->fetchAll(PDO::FETCH_ASSOC);
+    }
+    ?>
